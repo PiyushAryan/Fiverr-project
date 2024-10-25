f39dce3f98343b047594f9c1fc3618757a70f5f5
Piyush Aryan <piyusharyan81@gmail.com>
2024-10-25 19:39:11 +0530

    feat:add background stars and globe

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,16 @@
 import "@/styles/globals.css";
+import {Inter } from 'next/font/google'
+import localFont from "next/font/local";
+
+// const birthstone = localFont({ src: './BirthstonePro.woff' })
+const inter = Inter({ subsets: ['latin'] })
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+  <main className={inter.className}>
+
+  <Component {...pageProps} />;
+</main>
+  )
 }
